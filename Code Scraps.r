7af805504f5c1944033792d663186bf7ae4e832c
James Schroeder <james.schroeder@marquette.edu>
2015-07-08 19:03:34 -0500

    Added F0 Averaging

---
+++ Code Scraps.r
@@ -0,0 +1,5 @@
+# <- c(1:5)
+	# ee <- c("ee1", "ee2", "ee3", "ee4", "ee5")
+	# uu <- c("uu1", "uu2", "uu3", "uu4", "uu5")
+	# ah <- c("ah1", "ah2", "ah3", "ah4", "ah5")	
+	# "er" "aw" "uh" "oh" "eh" "ih" "oo" "ay" "ae"
